b9dbb4caf3c425e9787dcbaa9bcbd17ef831ff5d
wangli9142 <yuange0115@gmail.com>
2013-07-15 10:42:37 +0800

    new udpate

---
+++ package.js
@@ -0,0 +1,7 @@
+Package.describe({
+  summary: "custom-typeahead"
+});
+
+Package.on_use(function (api, where) {
+  api.add_files(['tests/lib/mockjax.js',  'tests/css/qunit.css', 'tests/lib/qunit.js', tests/lib/qunit.js'], 'js/bootstrap-typeahead.js', 'test/typeahead-tests.js','client');
+});
